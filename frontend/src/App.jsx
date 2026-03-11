aa26908f0eb0d6354a87b366f03aa04073c48e37
SimonH3-coder <simo16f4@elev.techcollege.dk>
2026-03-11 14:11:48 +0100

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
-import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return <Home />;
+  return <Navbar />;
 }
 
 export default App;
